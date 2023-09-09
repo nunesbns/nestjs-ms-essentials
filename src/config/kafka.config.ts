beccf309e6507c3bc09eedcb5ca97dbe454e8459
Bruno Nunes de Santana <nunes.inbox@gmail.com>
2023-09-09 17:04:18 -0300

    Fix kafka config classname

--- src/config/kafka.config.ts
+++ src/config/kafka.config.ts
@@ -4,12 +4,12 @@ import SchemaValidation, {
   ValidationConfig,
 } from 'src/utils/schemaValidation.util';
 
-interface kafkaConfigInterface {
+interface KafkaConfigInterface {
   brokers: Array<string>;
 }
 
-export default registerAs('kafka', (): kafkaConfigInterface => {
-  const configs: ValidationConfig<kafkaConfigInterface> = {
+export default registerAs('kafka', (): KafkaConfigInterface => {
+  const configs: ValidationConfig<KafkaConfigInterface> = {
     brokers: {
       value: JSON.parse(process.env.KAFKA_BROKERS),
       schema: Joi.array().required(),
