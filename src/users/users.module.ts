5c56f2835951fe5719134877d48a1b64c749fb38
Bruno Nunes de Santana <nunes.inbox@gmail.com>
2023-09-09 17:06:20 -0300

    Add client module to users

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,9 +1,31 @@
 import { Module } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
+import { ClientsModule, Transport } from '@nestjs/microservices';
+import { ConfigService } from '@nestjs/config';
 
 @Module({
   controllers: [UsersController],
   providers: [UsersService],
+  imports: [
+    ClientsModule.registerAsync([
+      {
+        name: 'USERS_SERVICE',
+        useFactory: (configService: ConfigService) => {
+          return {
+            transport: Transport.RMQ,
+            options: {
+              urls: configService.get<string[]>('rmq.hosts'),
+              queue: 'users_queue',
+              queueOptions: {
+                durable: configService.get<boolean>('rmq.durable'),
+              },
+            },
+          };
+        },
+        inject: [ConfigService],
+      },
+    ]),
+  ],
 })
 export class UsersModule {}
